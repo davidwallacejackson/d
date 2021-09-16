59236e39633b50f0a308213c291ac60581da0405
David Jackson <davidjackson@Davids-MacBook-Pro.local>
2021-09-15 19:47:11 -0700

    Remove contains() polyfill so we can use this with Vite

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 "use strict";
 
-var isValue         = require("type/value/is")
-  , isPlainFunction = require("type/plain-function/is")
-  , assign          = require("es5-ext/object/assign")
-  , normalizeOpts   = require("es5-ext/object/normalize-options")
-  , contains        = require("es5-ext/string/#/contains");
+var isValue = require("type/value/is"),
+	isPlainFunction = require("type/plain-function/is"),
+	assign = require("es5-ext/object/assign"),
+	normalizeOpts = require("es5-ext/object/normalize-options");
+// , contains        = require("es5-ext/string/#/contains");
 
-var d = (module.exports = function (dscr, value/*, options*/) {
+var d = (module.exports = function (dscr, value /*, options*/) {
 	var c, e, w, options, desc;
 	if (arguments.length < 2 || typeof dscr !== "string") {
 		options = value;
@@ -16,9 +16,9 @@ var d = (module.exports = function (dscr, value/*, options*/) {
 		options = arguments[2];
 	}
 	if (isValue(dscr)) {
-		c = contains.call(dscr, "c");
-		e = contains.call(dscr, "e");
-		w = contains.call(dscr, "w");
+		c = String.prototype.contains.call(dscr, "c");
+		e = String.prototype.contains.call(dscr, "e");
+		w = String.prototype.contains.call(dscr, "w");
 	} else {
 		c = w = true;
 		e = false;
@@ -28,7 +28,7 @@ var d = (module.exports = function (dscr, value/*, options*/) {
 	return !options ? desc : assign(normalizeOpts(options), desc);
 });
 
-d.gs = function (dscr, get, set/*, options*/) {
+d.gs = function (dscr, get, set /*, options*/) {
 	var c, e, options, desc;
 	if (typeof dscr !== "string") {
 		options = set;
@@ -50,8 +50,8 @@ d.gs = function (dscr, get, set/*, options*/) {
 		set = undefined;
 	}
 	if (isValue(dscr)) {
-		c = contains.call(dscr, "c");
-		e = contains.call(dscr, "e");
+		c = String.prototype.contains.call(dscr, "c");
+		e = String.prototype.contains.call(dscr, "e");
 	} else {
 		c = true;
 		e = false;
